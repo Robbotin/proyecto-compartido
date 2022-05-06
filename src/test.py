4458046c0af8738d5b7dd5b2a92e46fd9d4ed53d
amandabonaque <amandabonaque@gmail.com>
2022-05-06 11:42:07 +0200

    Terminado fichero Impuestos

---
+++ src/test.py
@@ -0,0 +1,6 @@
+from datetime import date
+from impuesto import Impuesto
+from Impuestos import Impuestos
+
+iva_superreducido_anterior = Impuesto(
+   1, 'IVA superreducido', 4.0, date(2010, 7, 1), date (2012, 8, 31))
